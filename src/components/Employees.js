babeddf4911bd39a7bb15b4ef4c47a41afbe255c
Masroor <musroor.q@gmail.com>
2023-05-10 19:07:29 +0500

    First Commit

---
+++ src/components/Employees.js
@@ -0,0 +1,24 @@
+const Employees = [
+  {
+    id: "1",
+    Name: "Qureshi",
+    Age: "25",
+  },
+  {
+    id: "2",
+    Name: "Magray",
+    Age: "26",
+  },
+  {
+    id: "3",
+    Name: "Khawaja",
+    Age: "27",
+  },
+  {
+    id: "4",
+    Name: "Khan",
+    Age: "28",
+  },
+];
+
+export default Employees;
